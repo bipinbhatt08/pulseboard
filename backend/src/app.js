42673f72171d07715588cf70d09b1a82ff48c57d
Bipin Bhatt <bipinbhatt280@gmail.com>
2026-05-14 18:04:48 -0400

    add start command and health route

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,11 +18,14 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 
+app.use('/health',(req,res)=>res.json({message:"good so far"}))
+
 app.use('/api/auth',authRouter)
 app.use('/api/polls',pollRouter)
 app.use('/api/questions',questionRouter)
 app.use('/api/votes',voteRouter)
 app.use('/api/options',optionRouter)
+
 
 
 app.use(globalErrorHandler)
