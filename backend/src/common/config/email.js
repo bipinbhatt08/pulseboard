546e1755049eb5ca7278089c189734ea815cc7c3
Bipin Bhatt <bipinbhatt280@gmail.com>
2026-05-16 00:10:31 -0400

    email config

--- backend/src/common/config/email.js
+++ backend/src/common/config/email.js
@@ -2,14 +2,15 @@ import nodemailer from "nodemailer"
 
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-  
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT || 587,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
-  },
-});
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT) || 587,
+    secure: false,  
+    requireTLS: true,  
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS,
+    },
+})
 
 
 
